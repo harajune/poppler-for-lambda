d467d84433a921df3c77a0849bcbb5802127a797
EC2 Default User <ec2-user@ip-172-31-31-52.us-west-2.compute.internal>
2018-01-15 16:14:46 +0000

    TODO: poppler build fails with lcms.h

--- build.sh
+++ build.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
 sudo yum -y groupinstall "Development Tools"
+sudo yum -y install gperf nss-devel libpng-devel libjpeg-devel libtiff-devel
 
 mkdir -p ~/tmp/{usr,etc,var,libs,install,downloads,tar}
 
 wget -P ~/tmp/downloads \
-         http://downloads.sourceforge.net/freetype/freetype-2.6.1.tar.bz2 \
-         http://www.freedesktop.org/software/fontconfig/release/fontconfig-2.11.1.tar.bz2 \
-         http://xmlsoft.org/sources/libxml2-2.9.2.tar.gz \
-         http://poppler.freedesktop.org/poppler-0.37.0.tar.xz \
+         http://downloads.sourceforge.net/freetype/freetype-2.9.tar.bz2 \
+         http://www.freedesktop.org/software/fontconfig/release/fontconfig-2.12.6.tar.bz2 \
+         https://cmake.org/files/v3.10/cmake-3.10.1.tar.gz \
+         http://xmlsoft.org/sources/libxml2-2.9.7.tar.gz \
+         http://poppler.freedesktop.org/poppler-0.62.0.tar.xz \
 && ls ~/tmp/downloads/*.tar.* | xargs -i tar xf {} -C ~/tmp/libs/
 
 pushd .
+
+####################################
+cd ~/tmp/libs/cmake*
+./cnofigure
+make
+sudo make install
+
+####################################
+
 
 ####################################
 cd ~/tmp/libs/freetype*
@@ -40,7 +51,7 @@ export FONTCONFIG_PKG=`pwd`
 PKG_CONFIG_PATH=~/tmp/usr/lib/pkgconfig/:$PKG_CONFIG_PATH \
 ./configure --prefix=/home/ec2-user/tmp/usr        \
             --sysconfdir=/home/ec2-user/tmp/etc    \
-            --localstatedir=/var \
+            --localstatedir=/home/ec2-user/tmp/var \
             --disable-docs       \
             --enable-libxml2 &&
 make
@@ -48,15 +59,27 @@ make install
 
 ####################################
 cd ~/tmp/libs/poppler*
+#PKG_CONFIG_PATH=~/tmp/usr/lib/pkgconfig/:$FONTCONFIG_PKG:$PKG_CONFIG_PATH \
+#./configure --prefix=/var/task      \
+#            --sysconfdir=/var/task/etc           \
+#            --enable-build-type=release \
+#            --enable-cmyk               \
+#            --enable-xpdf-headers && make
+mkdir -p build
+cd build
 PKG_CONFIG_PATH=~/tmp/usr/lib/pkgconfig/:$FONTCONFIG_PKG:$PKG_CONFIG_PATH \
-./configure --prefix=/var/task      \
-            --sysconfdir=/var/task/etc           \
-            --enable-build-type=release \
-            --enable-cmyk               \
-            --enable-xpdf-headers && make
-
+cmake .. -DCMAKE_BUILD_TYPE=Release   \
+       -DCMAKE_INSTALL_PREFIX=/home/ec2-user/tmp/usr  \
+       -DSYSCONFDIR=/var/task/etc \
+       -DSPLASH_CMYK=ON \
+       -DTESTDATADIR=$PWD/testfiles \
+       -DENABLE_XPDF_HEADERS=ON     \
+       -DENABLE_LIBOPENJPEG=none  \
+       -DENABLE_CMS=none  \
+&& cd .. && make &&
 make install DESTDIR="/home/ec2-user/tmp/install"
 
+NFIG_LIBRARIES
 unset FONTCONFIG_PKG
 popd
 
@@ -65,4 +88,4 @@ tar -C ~/tmp/install/var/task \
     --exclude='share'   \
     -zcvf ~/tmp/tar/poppler.tar.gz .
 
-aws s3 cp ~/tmp/tar/poppler.tar.gz s3://"${S3BUCKET}"/poppler.tar.gz
+# aws s3 cp ~/tmp/tar/poppler.tar.gz s3://"${S3BUCKET}"/poppler.tar.gz
